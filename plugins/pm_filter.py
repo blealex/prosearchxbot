d13575ebffea704a5902f06b11891de0a6d04aa8
blealex <98812061+blealex@users.noreply.github.com>
2022-05-09 17:49:13 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,8 +404,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('🎥🍿 Updates Channel', url='https://t.me/Movie_Collections_Bot')],[
-            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/Movie_Collections_Bot/1220')
+            InlineKeyboardButton('🎥🍿 Updates Channel', url='https://t.me/Lordship_series')],[
+            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/Lordship_series/2851')
            
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -433,7 +433,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/Movie_Collections_Bot'),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/Lordship_series'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
